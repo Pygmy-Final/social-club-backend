541c64de4e0e7d7f29af1eed0ce961d681e01967
AbrarAlzubaidi <abraralzobaide98@gmail.com>
2022-01-04 14:02:32 +0200

    null

--- events/models.py
+++ events/models.py
@@ -11,7 +11,7 @@ class Event(models.Model):
     EventCategory    = MultiSelectField(max_length=26, choices=[  ('Reading', 'Reading'), ('Cycling', 'Cycling'),
                                                                   ('Hiking','Hiking'), ('Drawing', 'Drawing'),('Photography', 'Photography'),
                                                                   ('Swimming','Swimming'),('Sleeping','Sleeping'),('Sports','Sports'),('Gaming','Gaming')])
-    EventStartTime  = models.DateTimeField()
+    EventStartTime  = models.DateTimeField(null=True)
     EventCreator    = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, null=True)
     EventParticipants = ListTextField(  base_field=IntegerField(),
                                         size=50,  null=True, default = 'None') 
